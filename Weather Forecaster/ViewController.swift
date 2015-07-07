366cf9ff2aa8bef4945db993248af3356332d0b4
mtb.talha <talha.babar@arbisoft.com.pk>
2015-07-07 15:02:52 +0500

    Weather Forecaster

---
+++ Weather Forecaster/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Weather Forecaster
+//
+//  Created by Talha Babar on 7/7/15.
+//  Copyright (c) 2015 Talha Babar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Weather ForecasterTests/Weather_ForecasterTests.swift
@@ -0,0 +1,36 @@
+//
+//  Weather_ForecasterTests.swift
+//  Weather ForecasterTests
+//
+//  Created by Talha Babar on 7/7/15.
+//  Copyright (c) 2015 Talha Babar. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Weather_ForecasterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
